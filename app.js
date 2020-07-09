d8de0991de543924281dce1b55b3552d979a238b
anthony soon <anthonychansoon@gmail.com>
2020-07-09 11:41:32 -0700

    express boiler plate

---
+++ app.js
@@ -0,0 +1,26 @@
+require('dotenv').config();
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const mongoose = require("mongoose");
+
+const app = express();
+
+app.set("view engine", "ejs");
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(express.static(__dirname + "/public"));
+
+mongoose.connect("mongodb://localhost:27017/SOPsDB", { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false })
+// mongoose.connect("mongodb+srv://admin-anthony:Test123@cluster0-jxcje.mongodb.net/mPlanDB", { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false })
+
+
+let port = process.env.PORT;
+if(port == null || port == "") {
+    port = 3000;
+}
+
+app.listen(port, function() {
+    console.log("server started on port = " + port)
+})
