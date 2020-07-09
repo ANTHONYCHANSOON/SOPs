f1e6128b2bdfc1cac07c481e51669c9ee0ecf137
anthony soon <anthonychansoon@gmail.com>
2020-07-09 15:32:11 -0700

    added dashboard files and passport login and register

--- app.js
+++ app.js
@@ -24,32 +24,32 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 mongoose.connect("mongodb://localhost:27017/SOPsDB", { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false })
-// mongoose.connect("mongodb+srv://admin-anthony:Test123@cluster0-jxcje.mongodb.net/mPlanDB", { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false })
+// mongoose.connect("mongodb+srv://admin-anthony:Test123@cluster0-jxcje.mongodb.net/SOPsDB", { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false })
 
 const userSchema = new mongoose.Schema({
     username: {
         type: String,
-        required : true
+        required: true
     },
-    password : {
-        type : String,
-        require : true
+    password: {
+        type: String,
+        require: true
     },
-    firstName : {
-        type : String,
-        require : true
+    firstName: {
+        type: String,
+        require: true
     },
-    lastName : {
-        type : String,
-        require : true
+    lastName: {
+        type: String,
+        require: true
     },
-    superUser : {
-        type : String,
-        default : "false"
+    superUser: {
+        type: String,
+        default: "false"
     },
-    dateCreated : {
-        type : Date,
-        default : Date.now(),
+    dateCreated: {
+        type: Date,
+        default: Date.now(),
 
     }
 });
@@ -58,6 +58,7 @@ userSchema.plugin(passportLocalMongoose);
 
 const User = new mongoose.model("User", userSchema);
 
+passport.use(User.createStrategy());
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
@@ -71,8 +72,44 @@ app.get("/login", (req, res) => {
     res.render("login");
 });
 
-app.post("/login", function (req, res) {
+app.get("/dashboard", function (req, res) {
+    if (req.isAuthenticated()) {
+        res.render("dashboard");
+    } else {
+        res.redirect("login");
+    }
+})
 
+app.post("/registerNewUser", function (req, res) {
+    console.log(req.body);
+    User.register({ username: req.body.username, firstName: req.body.fname, lastName: req.body.lname, superUser: req.body.superUser }, req.body.password, function (err, user) {
+        if (err) {
+            console.log(err)
+        } else {
+            passport.authenticate("local")(req, res, function () {
+                res.redirect("/dashboard");
+            })
+        }
+    })
+});
+
+app.post("/login", function (req, res) {
+    const user = new User({
+        username: req.body.username,
+        password: req.body.password
+    });
+    //console.log(user);
+    req.login(user, function (err) {
+        if (err) {
+            console.log(err);
+        } else {
+            passport.authenticate("local")(req, res, function () {
+                res.render("dashboard", {
+                    userinfo : req.user
+                })
+            })
+        }
+    })
 });
 
 let port = process.env.PORT;
